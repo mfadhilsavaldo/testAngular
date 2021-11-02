a457538071051706aaaed88ff65c0d82e514c070
TaqiyuddinJ <taqiyuddin.j@gmail.com>
2021-11-02 15:59:35 +0700

    update routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    
+    path: '',
+    component: DashboardComponent,
+    children:[
+      {
+        path: 'master-data',
+        loadChildren: () => import('./master-data/master-data.module').then(m=>m.MasterDataModule)
+        
+      }
+    ]
+  }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
